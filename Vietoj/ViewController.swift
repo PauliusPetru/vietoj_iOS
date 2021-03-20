7dab7ab53ac180299b8eb4d4553c975615b49dd1
Paulius <paulius.petru@gmail.com>
2021-03-20 16:43:58 +0200

    init

---
+++ Vietoj/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Vietoj
+//
+//  Created by T1406 on 2021-03-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
